bdf602690ead68ccf0beb04373d86155c74ce2b0
Kaato <34705627+kuhiga@users.noreply.github.com>
2021-05-04 23:14:44 -1000

    adding files

---
+++ .babelrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    ["@babel/preset-react", { targets: { node: "current" } }], // add this
+  ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-transform-runtime",
+  ],
+}
