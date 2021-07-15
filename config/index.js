6c5dd41d9f4ac6a39ea4711aed63336e1e62a6a5
Kaato <34705627+kuhiga@users.noreply.github.com>
2021-07-15 12:25:43 -0700

    typo

--- config/index.js
+++ config/index.js
@@ -98,11 +98,11 @@ module.exports = {
       // },
     ],
     button: {
-    //   useFileName: true,
-    //   name: "Resume",
-    //   fileName: "Kurt_Higa_Resume.pdf", // the file has to be placed inside the static folder at the root level
-    //   url: "", // if useFileName=false, you can set an anchor link here and use the button for navigational purposes
-    // },
+      //   useFileName: true,
+      //   name: "Resume",
+      //   fileName: "Kurt_Higa_Resume.pdf", // the file has to be placed inside the static folder at the root level
+      //   url: "", // if useFileName=false, you can set an anchor link here and use the button for navigational purposes
+    },
   },
   footerLinks: [
     // {
